f496c7f75529a70745d4c91a102ea880aa2382da
Kelly Powers <kpowers@PAV-MAC00431.local>
2020-11-18 20:19:23 -0500

    Done

--- account.js
+++ account.js
@@ -7,26 +7,30 @@ module.exports = function() {
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking == 1) {
+      // debugger;
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
     }
   }
+  // debugger;
 
   var signupBonus = 250,
-    deposit = 1000
+   deposit = 1000
 
   var openAccount = function(account, deposit) {
     account.balance += signupBonus
+    debugger
     output['New balance after signup bonus is '] = account.balance
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit)
   setAccountType()
+  // debugger;
   output['Account after opening'] = customerAccount
   return output
 }
---
+++ program.js
@@ -0,0 +1,7 @@
+var a = Math.floor(Math.random())
+debugger
+if (a>=0.5) {
+  console.log('head')
+} else {
+  console.log('tail')
+}
